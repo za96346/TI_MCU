a226117fb49aa248610dcbd750878fe9fed263e5
siou <za96346@gmail.com>
2024-04-10 22:34:14 +0800

    chore: 移除main.c 看不懂的

--- 期中考/OutOfBox_MSP-EXP430FR2355/main.c
+++ 期中考/OutOfBox_MSP-EXP430FR2355/main.c
@@ -7,7 +7,6 @@
 #include <jsmn.h>
 
 #include "lightsensor.h"
-#include "functiongenerator.h"
 
 /* Constants */
 #define MAX_STR_LEN         256
@@ -23,10 +22,6 @@ char txString[MAX_STR_LEN];
 void init_GPIO(void);
 void init_CS(void);
 void init_EUSCI(void);
-
-void transmitString(char *);
-void receiveString(char);
-static int jsoneq(const char *, jsmntok_t *, const char *);
 
 /**
  * main.c
@@ -138,168 +133,8 @@ void __attribute__ ((interrupt(ADC_VECTOR))) ADC_ISR (void)
 #error Compiler not supported!
 #endif
 {
-    switch(__even_in_range(ADCIV,ADCIV_ADCIFG))
-    {
-        case ADCIV_NONE:
-            break;
-        case ADCIV_ADCOVIFG:
-            break;
-        case ADCIV_ADCTOVIFG:
-            break;
-        case ADCIV_ADCHIIFG:
-            break;
-        case ADCIV_ADCLOIFG:
-            break;
-        case ADCIV_ADCINIFG:
-            break;
-        case ADCIV_ADCIFG:
-            lightsensor_ADC_Result = ADCMEM0;
-            __bic_SR_register_on_exit(LPM3_bits);              // Sleep Timer Exits LPM3
-            break;
-        default:
-            break;
+    if (__even_in_range(ADCIV,ADCIV_ADCIFG) == ADCIV_ADCIFG) {
+        lightsensor_ADC_Result = ADCMEM0;
+        __bic_SR_register_on_exit(LPM3_bits);              // Sleep Timer Exits LPM3
     }
-}
-
-// EUSCI interrupt service routine
-#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
-#pragma vector=USCI_A1_VECTOR
-__interrupt void USCI_A1_ISR(void)
-#elif defined(__GNUC__)
-void __attribute__ ((interrupt(USCI_A1_VECTOR))) USCI_A1_ISR (void)
-#else
-#error Compiler not supported!
-#endif
-{
-    switch(__even_in_range(UCA1IV,USCI_UART_UCTXCPTIFG))
-    {
-        case USCI_NONE: break;
-        case USCI_UART_UCRXIFG:
-            functiongenerator_disable();
-            Timer_B_stop(TIMER_B0_BASE);
-            // Read buffer
-            receiveString(UCA1RXBUF);
-
-            if (rxStringReady)
-            {
-                int i;
-                int r;
-                jsmn_init(&p);
-                r = jsmn_parse(&p, rxString, strlen(rxString), t, sizeof(t)/sizeof(t[0]));
-
-                // Loop over keys of JSON object
-                for (i = 1; i < r; i++) {
-                    if (jsoneq(rxString, &t[i], "inputSignalSource") == 0) {
-                        // input signal source received from PC
-                        inputSignalSource = strtol( rxString+t[i+1].start, NULL, 10);
-                        if (inputSignalSource == 1)
-                            opAmpMode = 0;
-                        else if (inputSignalSource == 2)
-                            opAmpMode = 1;
-                        i++;
-                    }
-                    if (jsoneq(rxString, &t[i], "signalType") == 0) {
-                        // New Signal Frequency received from PC
-                        signalType = strtol( rxString+t[i+1].start, NULL, 10);
-                        i++;
-                    }
-                    if (jsoneq(rxString, &t[i], "signalFreq") == 0) {
-                        // New Signal Frequency received from PC
-                        signalFreq = strtol( rxString+t[i+1].start, NULL, 10);
-                        i++;
-                    }
-                    if (jsoneq(rxString, &t[i], "signalAmp") == 0) {
-                        // New Signal Amplitude received from PC
-                        signalAmp = strtol( rxString+t[i+1].start, NULL, 10);
-                        i++;
-                    }
-                    if (jsoneq(rxString, &t[i], "dacFreq") == 0) {
-                        // New DAC Sample Frequency received from PC
-                        dacFreq = strtol( rxString+t[i+1].start, NULL, 10);
-                        i++;
-                    }
-                    if (jsoneq(rxString, &t[i], "adcFreq") == 0) {
-                        // New ADC Sample Frequency received from PC
-                        adcFreq = strtol( rxString+t[i+1].start, NULL, 10);
-                        i++;
-                    }
-                    if (jsoneq(rxString, &t[i], "opAmpMode") == 0) {
-                        // New ADC Sample Frequency received from PC
-                        opAmpMode = strtol( rxString+t[i+1].start, NULL, 10);
-                        i++;
-                    }
-                    if (jsoneq(rxString, &t[i], "opAmpGain") == 0) {
-                        // New ADC Sample Frequency received from PC
-                        opAmpGain = strtol( rxString+t[i+1].start, NULL, 10);
-                        i++;
-                    }
-                    if (jsoneq(rxString, &t[i], "mode") == 0) {
-                        __bic_SR_register_on_exit(LPM3_bits);
-                        i++;
-                    }
-                }
-
-                Timer_B_startCounter(TIMER_B0_BASE, TIMER_B_UP_MODE);
-                lightsensor_init_ADC();
-
-                rxStringReady = false;
-            }
-            break;
-        case USCI_UART_UCTXIFG: break;
-        case USCI_UART_UCSTTIFG: break;
-        case USCI_UART_UCTXCPTIFG: break;
-        default: break;
-    }
-}
-
-// Transmits string buffer through EUSCI UART
-void transmitString(char *str)
-{
-    unsigned int i = 0;
-    for(i = 0; i < strlen(str); i++)
-    {
-        if (str[i] != 0)
-        {
-            // Transmit Character
-            while (EUSCI_A_UART_queryStatusFlags(EUSCI_A1_BASE, EUSCI_A_UART_BUSY));
-            EUSCI_A_UART_transmitData(EUSCI_A1_BASE, str[i]);
-        }
-    }
-}
-
-// Receives strings terminated with \n
-void receiveString(char data) {
-    static bool rxInProgress = false;
-    static unsigned int charCnt = 0;
-
-    if(!rxInProgress){
-        if ((data != '\n') ){
-            rxInProgress = true;
-            charCnt = 0;
-            rxString[charCnt] = data;
-        }
-    }else{ // in progress
-        charCnt++;
-        if((data != '\n')){
-            if (charCnt >= MAX_STR_LEN){
-                rxInProgress = false;
-            }else{
-                rxString[charCnt] = data;
-            }
-        }else{
-            rxInProgress = false;
-            rxString[charCnt] = '\0';
-            // String receive complete
-            rxStringReady = true;
-        }
-    }
-}
-
-// Compare JSON keys
-static int jsoneq(const char *json, jsmntok_t *tok, const char *s) {
-    if (tok->type == JSMN_STRING && (int) strlen(s) == tok->end - tok->start &&
-            strncmp(json + tok->start, s, tok->end - tok->start) == 0) {
-        return 0;
-    }
-    return -1;
 }
